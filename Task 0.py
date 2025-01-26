36e2bcc35965310119317d667a075fb61ab7748f
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2025-01-26 20:06:18 +0530

    commit

--- Task 0.py
+++ Task 0.py
@@ -8,7 +8,10 @@ font_path = "./Fonts/roboto.ttf"
 image_size = (256, 128)
 font_size = 32
 num_samples = 10
-words_list = ["hello", "world", "data", "ai", "model", "text"]
+words_list= []
+with open("Dictionary.txt", "r") as file:
+    words_list.extend(eval(file.read()))
+print("Number of words found in the Dictionary: ",len(words_list))
 # Source for the basic logic https://cloudinary.com/guides/image-effects/a-guide-to-adding-text-to-images-with-python
 os.makedirs(output_dir, exist_ok=True)
 
@@ -18,7 +21,7 @@ def create_image(word, font, size):
     text_bbox = draw.textbbox((0, 0), word, font=font)
     text_width, text_height = text_bbox[2] - text_bbox[0], text_bbox[3] - text_bbox[1]
     position = ((size[0] - text_width) // 2, (size[1] - text_height) // 2)
-    draw.text(position, word, font=font, fill="black")
+    draw.text(position, word.capitalize(), font=font, fill="black")
 
     return img
 
